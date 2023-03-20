e22cdfb4b6d03626658c8052d102d439c8069624
Andrey Carvajal <ancarsa.wasabi.12@gmail.com>
2023-03-20 16:57:00 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+// npm install -g @aws-amplify/cli
+// amplify configure
+// npm install aws-amplify @aws-amplify/ui-react to Add Authentication
+// amplify add auth   To create the authentication service,
+// amplify push --y to deploy the authentication service it will create a file called aws-exports.js located in the src directory of our project.
+// Add Athentication flow in App.js
 import logo from "./logo.svg";
-import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Wasabito w from V2</h1>
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
